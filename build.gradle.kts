5c234263574abf388f499eb151fd2095d4317be9
作佐部 太祐 <sakusabedaisuke@team-lab.com>
2020-05-04 21:02:21 +0900

    build.gradle にthymeleaf を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,11 @@ repositories {
     jcenter()
 }
 
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
